a427893a5582b17be2278fd237ed7721600a7e08
foxngao <minhtienst0@gmail.com>
2025-05-14 16:19:30 +0700

    Lab-01

---
+++ lab-01/Bai1.py
@@ -0,0 +1,5 @@
+# nhập tên và tuổi từng người dùng
+ten = input("Nhập tên của bạn: ")
+tuoi = input("Nhập tuổi của bạn: ")
+# In Thông điệp chào mừng vời thông tin vừa nhập
+print("chào mừng", ten,"! Bạn", tuoi, "tuổi.")
