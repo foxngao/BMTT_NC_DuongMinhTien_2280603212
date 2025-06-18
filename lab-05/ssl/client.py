1399f749fa8c03cded34ab23ee8ef4d895eaa182
foxngao <minhtienst0@gmail.com>
2025-06-18 16:12:14 +0700

    lab-05

--- lab-05/ssl/client.py
+++ lab-05/ssl/client.py
@@ -2,10 +2,8 @@ import socket
 import ssl
 import threading
 
-
 # Thông tin server
 server_address = ('localhost', 12345)
-
 
 def receive_data(ssl_socket):
     try:
@@ -17,34 +15,31 @@ def receive_data(ssl_socket):
     except:
         pass
     finally:
+        print("Kết nối đã đóng!")
         ssl_socket.close()
-        print("Kết nối đã đóng.")
-
 
 # Tạo socket client
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-
 # Tạo SSL context
-context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+context = ssl.SSLContext(ssl.PROTOCOL_TLS)
 context.verify_mode = ssl.CERT_NONE  # Change this according to your needs
 context.check_hostname = False  # Change this according to your needs
-
 
 # Thiết lập kết nối SSL
 ssl_socket = context.wrap_socket(client_socket, server_hostname='localhost')
 ssl_socket.connect(server_address)
 
-
 # Bắt đầu một luồng để nhận dữ liệu từ server
 receive_thread = threading.Thread(target=receive_data, args=(ssl_socket,))
 receive_thread.start()
 
-
-# Gửi dữ liệu từ client
+# Gửi và nhận dữ liệu từ server
 try:
     while True:
-        message = input("Nhập tin nhắn: ")
+        message = input("Nhập thông tin: ")
         ssl_socket.send(message.encode('utf-8'))
 except KeyboardInterrupt:
+    pass
+finally:
     ssl_socket.close()
